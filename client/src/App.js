51a98ea0c11c9c2cb6e2f9bc3a2427522aa0c302
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-07-30 23:21:22 +0300

    protected route and axios setups

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setUser } from "./store/slicers/user";
+
+import { ProtectedRoute } from "./components/ProtectedRoute";
 import NavigationBar from "./components/NavigationBar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Trades from "./containers/Trades";
 import Backtests from "./containers/Backtests";
 import Profile from "./containers/Profile";
 import Dashboard from "./containers/Dashboard";
 import Settings from "./containers/Settings";
+import LoginPage from "./containers/LoginPage";
 
 function App() {
+  let user = localStorage.getItem("user");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (user) {
+      dispatch(setUser(user));
+    }
+  }, [user]);
+
+  const protectedRouteController = (component) => {
+    return <ProtectedRoute user={user}>{component}</ProtectedRoute>;
+  };
+
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
           <Route path="/" element={<NavigationBar />}>
-            <Route index element={<Dashboard />} />
-            <Route path="/trades" element={<Trades />} />
-            <Route path="/backtests" element={<Backtests />} />
+            <Route index element={protectedRouteController(<Dashboard />)} />
+            <Route
+              path="/trades"
+              element={protectedRouteController(<Trades />)}
+            />
+            <Route
+              path="/backtests"
+              element={protectedRouteController(<Backtests />)}
+            />
             <Route path="/profile/:id" element={<Profile />} />
-            <Route path="/settings" element={<Settings />} />
+            <Route
+              path="/settings"
+              element={protectedRouteController(<Settings />)}
+            />
           </Route>
+          <Route path="login" element={<LoginPage />} />
         </Routes>
       </div>
     </BrowserRouter>
--- client/src/store/slicers/user.js
+++ client/src/store/slicers/user.js
@@ -9,7 +9,7 @@ export const userSlice = createSlice({
   initialState,
   reducers: {
     setUser: (state, action) => {
-      state.user = action.payload;
+      state.currentUser = action.payload;
     },
   },
 });
